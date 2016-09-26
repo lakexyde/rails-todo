bedc18e0d0585f099b3453031e2c1fb404ffb547
Olalekan Oladipupo <lakesida10@gmail.com>
2016-09-26 02:03:19 +0100

    Add styles and structures

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,13 +1,14 @@
 class TodoItemsController < ApplicationController
 	before_action :set_todo_list
+	before_action :set_todo_item, except: [:create]
   def create
 	@todo_item = @todo_list.todo_items.create(todo_items_params)
 	if @todo_item.save
       redirect_to @todo_list
      end
   end
+
   def destroy
-  	@todo_item = @todo_list.todo_items.find(params[:id])
   	if @todo_item.destroy
   		flash[:success] = "Todo list item has been successfully deleted"
   	else
@@ -15,11 +16,23 @@ class TodoItemsController < ApplicationController
   	end
   	redirect_to @todo_list
   end
+
+  def complete
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "Todo item completed"
+
+  end
   private
+
+  def set_todo_item
+  	@todo_item = @todo_list.todo_items.find(params[:id])
+
+  end
 
   def set_todo_list
   	@todo_list = TodoList.find(params[:todo_list_id])
   end
+
   def todo_items_params
   	params.require(:todo_item).permit(:content)
   end
